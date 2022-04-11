2db57137a4fd898de5f85dc613d9647bd3cac035
hasnatulhaq <hasnatulhaq@outlook.com>
2022-04-11 14:46:48 +0500

    updating

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -9,6 +9,8 @@
     import {Room} from '@mui/icons-material'
     import './Map.css'
     import Cities from '../../cities';
+    import randomColor from "randomcolor";
+import { yellow } from "@mui/material/colors";
 
 
  
@@ -183,33 +185,99 @@
 //            "line-width": 5,
 // }
 //         }
-     const zonedata=['#ce2c69','R10A']   
+const datacode = [
+  { 'code': 'red', },
+  { 'code': 'C1-6',},
+  { 'code': 'C1-6A',},
+  { 'code': 'C1-7',},
+  { 'code': 'C1-7A',},
+  { 'code': 'C1-8',},
+  { 'code': 'C1-8A',},
+  { 'code': 'C1-8X',},
+  { 'code': 'C1-9' ,},
+  { 'code': 'C1-9A',},
+  { 'code': 'C2-6' ,},
+  { 'code': 'C2-6A',},
+  { 'code': 'C2-7',},
+  { 'code': 'C2-7A',},
+  { 'code': 'C2-8' ,},
+  { 'code': 'C2-8A',},
+  { 'code': 'C3',},
+  { 'code': 'C3A',}
+  ];
+// const data = [
+//   { 'code': 'ROU', 'hdi': 0.811 },
+//   { 'code': 'RUS', 'hdi': 0.816 },
+//   { 'code': 'SRB', 'hdi': 0.787 },
+//   { 'code': 'SVK', 'hdi': 0.855 },
+//   { 'code': 'SVN', 'hdi': 0.896 },
+//   { 'code': 'ESP', 'hdi': 0.891 },
+//   { 'code': 'SWE', 'hdi': 0.933 },
+//   { 'code': 'CHE', 'hdi': 0.944 },
+//   { 'code': 'HRV', 'hdi': 0.831 },
+//   { 'code': 'CZE', 'hdi': 0.888 },
+//   { 'code': 'DNK', 'hdi': 0.929 },
+//   { 'code': 'EST', 'hdi': 0.871 },
+//   { 'code': 'FIN', 'hdi': 0.92 },
+//   { 'code': 'FRA', 'hdi': 0.901 },
+//   { 'code': 'DEU', 'hdi': 0.936 },
+//   { 'code': 'GRC', 'hdi': 0.87 },
+//   { 'code': 'ALB', 'hdi': 0.785 },
+//   { 'code': 'AND', 'hdi': 0.858 },
+//   { 'code': 'AUT', 'hdi': 0.908 },
+//   { 'code': 'BLR', 'hdi': 0.808 },
+//   { 'code': 'BEL', 'hdi': 0.916 },
+//   { 'code': 'BIH', 'hdi': 0.768 },
+//   { 'code': 'BGR', 'hdi': 0.813 },
+//   { 'code': 'MKD', 'hdi': 0.757 },
+//   { 'code': 'MLT', 'hdi': 0.878 },
+//   { 'code': 'MDA', 'hdi': 0.7 },
+//   { 'code': 'MNE', 'hdi': 0.814 },
+//   { 'code': 'NLD', 'hdi': 0.931 },
+//   { 'code': 'NOR', 'hdi': 0.953 },
+//   { 'code': 'POL', 'hdi': 0.865 },
+//   { 'code': 'PRT', 'hdi': 0.847 },
+//   { 'code': 'HUN', 'hdi': 0.838 },
+//   { 'code': 'ISL', 'hdi': 0.935 },
+//   { 'code': 'IRL', 'hdi': 0.938 },
+//   { 'code': 'ITA', 'hdi': 0.88 },
+//   { 'code': 'LVA', 'hdi': 0.847 },
+//   { 'code': 'LIE', 'hdi': 0.916 },
+//   { 'code': 'LTU', 'hdi': 0.858 },
+//   { 'code': 'LUX', 'hdi': 0.904 },
+//   { 'code': 'UKR', 'hdi': 0.751 },
+//   { 'code': 'GBR', 'hdi': 0.922 }
+//   ];   
+            const zonecolor= randomColor();
+    //   const matchExpression = ['match', ['get','color']];
+ 
+    //  for (const row of datacode) {
+
+    //   const color = randomColor();
+    //   console.log(color);
+    //   matchExpression.push(row['code'], color);
+    //   }
+    //  matchExpression.push('rgba(0, 0, 0, 0)');
 
 const layerStyle={
             id:'zoneomics', 
-            source:'zoneomics',
-           type: 'fill',
+            type: 'fill',
+            source : 'zoneomics',
           'source-layer': 'zones',
-           paint: {
-             "fill-color": {
-              property: 'density', // this will be your density property form you geojson
-              stops: [
-                    [0, 'red'],
-                    [1, 'green'],
-                    [2, 'yellow'],
-                    [3, 'black'],
-                    [4, 'orange'],
-                    [5, 'pink'],
-                    [6, 'indigo'],
-                    [7, 'purple'],
-                    [8, 'green'],
-              ]
+          'paint': {
+            'fill-color': ['match', ['get', 'id'], // get the property
+                           'BPC', 'yellow',              // if 'GP' then yellow
+                           'C1-6', 'black', 
+                           'C1-6A' , 'red',              // if 'XX' then black 
+                           'orange'] ,
+                           'fill-outline-color': 'red',                    // white otherwise
             },
-             'fill-outline-color': 'red',
-             "fill-opacity": 0.5,
-           },
           }
-
+          // paint: {
+          //   "fill-color": zonecolor,
+          //   'fill-outline-color': 'red',
+          //   "fill-opacity": 0.5,
+          // },
 
             //  "fill-color": {
             //   property: 'percentile',
@@ -225,49 +293,7 @@ const layerStyle={
             //     ['C1-9', 'green'],
             //   ]
             //  },
-            const data = [
-              { 'code': 'ROU', 'hdi': 0.811 },
-              { 'code': 'RUS', 'hdi': 0.816 },
-              { 'code': 'SRB', 'hdi': 0.787 },
-              { 'code': 'SVK', 'hdi': 0.855 },
-              { 'code': 'SVN', 'hdi': 0.896 },
-              { 'code': 'ESP', 'hdi': 0.891 },
-              { 'code': 'SWE', 'hdi': 0.933 },
-              { 'code': 'CHE', 'hdi': 0.944 },
-              { 'code': 'HRV', 'hdi': 0.831 },
-              { 'code': 'CZE', 'hdi': 0.888 },
-              { 'code': 'DNK', 'hdi': 0.929 },
-              { 'code': 'EST', 'hdi': 0.871 },
-              { 'code': 'FIN', 'hdi': 0.92 },
-              { 'code': 'FRA', 'hdi': 0.901 },
-              { 'code': 'DEU', 'hdi': 0.936 },
-              { 'code': 'GRC', 'hdi': 0.87 },
-              { 'code': 'ALB', 'hdi': 0.785 },
-              { 'code': 'AND', 'hdi': 0.858 },
-              { 'code': 'AUT', 'hdi': 0.908 },
-              { 'code': 'BLR', 'hdi': 0.808 },
-              { 'code': 'BEL', 'hdi': 0.916 },
-              { 'code': 'BIH', 'hdi': 0.768 },
-              { 'code': 'BGR', 'hdi': 0.813 },
-              { 'code': 'MKD', 'hdi': 0.757 },
-              { 'code': 'MLT', 'hdi': 0.878 },
-              { 'code': 'MDA', 'hdi': 0.7 },
-              { 'code': 'MNE', 'hdi': 0.814 },
-              { 'code': 'NLD', 'hdi': 0.931 },
-              { 'code': 'NOR', 'hdi': 0.953 },
-              { 'code': 'POL', 'hdi': 0.865 },
-              { 'code': 'PRT', 'hdi': 0.847 },
-              { 'code': 'HUN', 'hdi': 0.838 },
-              { 'code': 'ISL', 'hdi': 0.935 },
-              { 'code': 'IRL', 'hdi': 0.938 },
-              { 'code': 'ITA', 'hdi': 0.88 },
-              { 'code': 'LVA', 'hdi': 0.847 },
-              { 'code': 'LIE', 'hdi': 0.916 },
-              { 'code': 'LTU', 'hdi': 0.858 },
-              { 'code': 'LUX', 'hdi': 0.904 },
-              { 'code': 'UKR', 'hdi': 0.751 },
-              { 'code': 'GBR', 'hdi': 0.922 }
-              ];
+          
             
         return(
         <>
