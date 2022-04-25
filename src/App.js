2767b0d80d60cdb73cb5ac614945ff16c756ecaf
hasnatulhaq <hasnatulhaq@outlook.com>
2022-04-25 11:55:35 +0500

    place searching

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import './App.css';
-import Mapro from './Components/Map/Map.js';
-//import Search from './Components/search/Search';
-import Sidbar from './Components/shared/Sidebar/Sidbar';
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, { useState, useRef, useCallback } from "react";
+import { render } from "react-dom";
+import MapGL from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
 
+// Please be a decent human and don't abuse my Mapbox API token.
+// If you fork this sandbox, replace my API token with your own.
+// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiaGFzbmF0dWxoYXEiLCJhIjoiY2wwdzBjb3JrMTc3ajNkbjUyaDljbG8zcyJ9.zR9o-L0WGPt1JKTHd0oUFg";
 
-function App() {
-  
+export default function App ()  {
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides
+      });
+    },
+    [handleViewportChange]
+  );
+
   return (
-    <div className="App">
-      {/* <div className='searh_div'>
-        <div>
-        <Search/>
-        </div>
-        <div className='navbar_items'>
-          <a>Home</a>
-          <a>About</a>
-        </div>
-      </div> */}
-      <div className='main_container'></div>
-      <div className='side_nav'>
-          <Sidbar/>
-      </div>
-      <div className="map_div">
-      <Mapro/>
-      </div>
+    <div style={{ height: "100vh" }}>
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        width="100%"
+        height="100%"
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
+      </MapGL>
     </div>
   );
-}
-export default App;
+};
